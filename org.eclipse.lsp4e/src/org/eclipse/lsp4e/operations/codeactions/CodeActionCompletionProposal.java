5ac7de36925ea18791841659b84743bf9d43fd21
Ahmed Neil Hussain <ahmed@cocotec.io>
2026-08-27 16:31:18 +0100

    Code Actions: Require explicit support of codeAction/resolve
    The server can advertise support for code actions by setting 'true'
    in the server options, or can give more fine-grained support by
    supplying a CodeActionOptions (which includes a resolveProvider boolean).
    We were assuming that the coarse support implies support for codeAction/resolve,
    but the spec implies we should only do so if fine-grained options are present
    and specify true for that flag.
    
    Issue uncovered while investigating related bug #1565

--- org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/codeactions/CodeActionCompletionProposal.java
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/operations/codeactions/CodeActionCompletionProposal.java
@@ -49,14 +49,7 @@ public class CodeActionCompletionProposal implements ICompletionProposal {
 	static boolean isCodeActionResolveSupported(@Nullable ServerCapabilities capabilities) {
 		if (capabilities != null) {
 			Either<Boolean, CodeActionOptions> caProvider = capabilities.getCodeActionProvider();
-			if (caProvider.isLeft()) {
-				return caProvider.getLeft();
-			} else if (caProvider.isRight()) {
-				CodeActionOptions options = caProvider.getRight();
-				var resolveProvider = options.getResolveProvider();
-				if (resolveProvider != null)
-					return resolveProvider;
-			}
+			return caProvider.isRight() && Boolean.TRUE.equals(caProvider.getRight().getResolveProvider());
 		}
 		return false;
 	}
